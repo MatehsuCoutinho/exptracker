99f48b467065d4d0969ee7eb181692585e60603a
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-05-12 16:44:35 -0300

    cadastro (deu trabalho viu)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user.model')
 const jwt = require('jsonwebtoken')
 
 //gerando token jwt
@@ -6,16 +7,46 @@ const generateToken = (id) => {
 }
 
 //registrando usuario 
-exports.registerUser = async (req, res) =>{
+exports.registerUser = async (req, res) => {
+    const { fullName, email, password, profileImageUrl } = req.body || {}
 
+    //conferindo campos vazios
+    if (!fullName || !email || !password ) {
+        return res.status(400).json({ message: "Todos os campos são necessários" })
+    }
+
+    // vendo se email existe
+    try {
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ message: "Email já está em uso" })
+        }
+
+        //criando user
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl,
+        })
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id)
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: 'Erro registrando usuário', error: err.message })
+    }
 }
 
 //logando usuario
-exports.loginUser = async (req, res) =>{
-    
+exports.loginUser = async (req, res) => {
+
 }
 
 // mostrar user
-exports.getUserInfo = async (req, res) =>{
-    
+exports.getUserInfo = async (req, res) => {
+
 }
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -19,3 +19,17 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 )
+
+// encriptar senha antes de salvar
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// comparar senhas
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password)
+}
+
+module.exports = mongoose.model('User', userSchema)

c216b8f005c4955458ebeb95a77e654c2c32212b
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-05-16 11:36:34 -0300

    chamando api login

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -11,23 +11,24 @@ import SignUp from './pages/auth/signup'
 import Home from './pages/dashboard/home'
 import Income from './pages/dashboard/income'
 import Expense from './pages/dashboard/expense'
-
-
+import UserProvider from './context/userContext'
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Root />} />
-          <Route path='/login' exact element={<Login />} />
-          <Route path='/signup' exact element={<SignUp />} />
-          <Route path='/dashboard' exact element={<Home />} />
-          <Route path='/income' exact element={<Income />} />
-          <Route path='/expense' exact element={<Expense />} />
-        </Routes>
-      </Router>
-    </div>
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Root />} />
+            <Route path='/login' exact element={<Login />} />
+            <Route path='/signup' exact element={<SignUp />} />
+            <Route path='/dashboard' exact element={<Home />} />
+            <Route path='/income' exact element={<Income />} />
+            <Route path='/expense' exact element={<Expense />} />
+          </Routes>
+        </Router>
+      </div>
+    </UserProvider>
   )
 }
 
--- frontend/expense-tracker/src/pages/auth/login.jsx
+++ frontend/expense-tracker/src/pages/auth/login.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
 import { Link, useNavigate } from 'react-router-dom'
 import Input from '../../components/layouts/inputs/Input'
 import { validateEmail } from '../../utils/helper'
+import axiosInstance from '../../utils/axios.instance'
+import { API_PATHS } from '../../utils/apiPaths'
+import { UserContext } from '../../context/userContext'
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState(null)
+
+  const { updateUser } = useContext(UserContext)
 
   const navigate = useNavigate()
 
@@ -28,7 +33,26 @@ const Login = () => {
     setError('')
 
     //chamar a api de login aq depois
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      })
+      const { token, user } = response.data
 
+      if (token) {
+        localStorage.setItem("token", token)
+        updateUser(user)
+        navigate('/dashboard')
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message)
+      }
+      else {
+        setError('Alguma coisa deu errado. Tente novamente mais tarde')
+      }
+    }
   }
 
   return (

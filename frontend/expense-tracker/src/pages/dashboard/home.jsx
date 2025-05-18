9bc75fa4378d0dd1d5799c7d259c537cbcc4cb3b
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-05-18 12:29:02 -0300

    front end side menu + ux ui + fetch das transacoes

--- frontend/expense-tracker/src/pages/dashboard/home.jsx
+++ frontend/expense-tracker/src/pages/dashboard/home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 import { useUserAuth } from '../../hooks/useUserAuth'
+import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axios.instance'
+import { API_PATHS } from '../../utils/apiPaths'
 
 const Home = () => {
   useUserAuth()
+
+  const navigate = useNavigate()
+
+  const [dashboardData, setDashboardData] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const fetchDashboardData = async () => {
+    if (loading) return
+
+    setLoading(true)
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      )
+
+      if (response.data) {
+        setDashboardData(response.data)
+      }
+    } catch (error) {
+      console.log('Alguma coisa deu errado. Tente novamente mais tarde', error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchDashboardData()
+    return () => { }
+  }, [])
+
 
   return (
     <DashboardLayout activeMenu='Dashboard'>

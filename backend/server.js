f5307483089b2ef960a4221ff7386baf45294fb4
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-05-25 12:04:34 -0300

    deploy back

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL,
+        origin: 'https://exptracker-kappa.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization']
     })

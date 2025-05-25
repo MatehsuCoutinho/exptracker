5da3dd1d1525c27fbe20ab18c86eb8157ae93582
Matheus Coutinho <matheuscoutinho.0420@gmail.com>
2025-05-25 13:21:13 -0300

    f

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,12 @@ app.use(
     })
 )
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use(express.json())
 
 connectDB()
